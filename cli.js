68f7a657a28e41479787dd1924e131ab78b6c216
Rishi Giri <rushforlinux@gmail.com>
2016-05-26 20:17:14 +0530

    handling errors

--- cli.js
+++ cli.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 'use strict';
 /* eslint-disable prefer-template */
 const meow = require('meow');
@@ -12,9 +13,32 @@ meow(`
 	  $ fast
 `);
 
+// dealing with connections in a better way
+function checkConnection(cb) {
+	require('dns').lookup('fast.com', err => {
+		if (err && err.code === 'ENOTFOUND') {
+			cb(false);
+		} else {
+			cb(true);
+		}
+	});
+}
+
+checkConnection(isConnected => {
+	if (isConnected) {
+		// no need, and even if we show the message, it won't print because of running spinners.
+	} else {
+		console.log(chalk.red('\n Please check you internet connection.\n'));
+		// exiting if the network is unreachable
+		process.exit(1);
+	}
+});
+
 let data = {};
 const spinner = ora();
-const speed = () => chalk[data.isDone ? 'green' : 'cyan'](data.speed + ' ' + chalk.dim(data.unit)) + '\n';
+
+// extra \n for equal gaping, providing it more evenness while showing the final message.
+const speed = () => chalk[data.isDone ? 'green' : 'cyan'](data.speed + ' ' + chalk.dim(data.unit)) + '\n\n';
 
 function exit() {
 	logUpdate('\n\n    ' + speed());
