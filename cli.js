13b73959cbe5fab24e41ad203f356bf0c2da1588
Rishi Giri <rushforlinux@gmail.com>
2016-05-30 17:38:32 +0700

    handling errors and improved output
    A little tweak to make this tool perfect.
    
    Well, I saw that, even if the system isn't connected to any network, the tool kept running and while stopping the process, it showed errors which kept ruining the beauty of this tool and the code. ( check the screenshot )
    Even the uneven gaps didn't look good.
    
    ![before](https://cloud.githubusercontent.com/assets/9411252/15578778/70e54bd8-237f-11e6-878a-44613663609d.png)
    
    So, I tweaked it little to make it even more good, showing messages and handling errors in a better way ( You can compare the screenshots )
    
    ![after](https://cloud.githubusercontent.com/assets/9411252/15578985/52d3e28e-2380-11e6-936d-0cd15d163ff1.png)
    
    Well, the downloading speed is quite low here..haha
    
    Closes #8

--- cli.js
+++ cli.js
@@ -5,6 +5,7 @@ const meow = require('meow');
 const chalk = require('chalk');
 const logUpdate = require('log-update');
 const ora = require('ora');
+const dns = require('dns');
 const api = require('./api');
 
 meow(`
@@ -12,9 +13,19 @@ meow(`
 	  $ fast
 `);
 
+// check connection
+dns.lookup('fast.com', err => {
+    if (err && err.code === 'ENOTFOUND') {
+        console.log(chalk.red('\n Please check you internet connection.\n'));
+        process.exit(1);
+    }
+});
+
 let data = {};
 const spinner = ora();
-const speed = () => chalk[data.isDone ? 'green' : 'cyan'](data.speed + ' ' + chalk.dim(data.unit)) + '\n';
+
+// extra \n for equal gaping, providing it more evenness while showing the final message.
+const speed = () => chalk[data.isDone ? 'green' : 'cyan'](data.speed + ' ' + chalk.dim(data.unit)) + '\n\n';
 
 function exit() {
 	logUpdate('\n\n    ' + speed());
