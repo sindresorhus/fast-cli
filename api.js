9d8644ac247a74b5d72e44de6f99de44e4ab6d48
Ben Southgate <bsouthga@gmail.com>
2016-08-09 17:39:46 -0400

    feat: api() => Observable, use jsdom

--- api.js
+++ api.js
@@ -1,40 +1,47 @@
 'use strict';
-/* eslint-env browser */
-const driver = require('promise-phantom');
-const phantomjs = require('phantomjs-prebuilt');
+const jsdom = require('jsdom');
+const Observable = require('zen-observable');
 
-function init(page, cb, prevSpeed) {
-	// TODO: doesn't work with arrow function. open issue on `promise-phantom`
-	page.evaluate(function () { // eslint-disable-line prefer-arrow-callback
-		const $ = document.querySelector.bind(document);
+module.exports = () => new Observable(observer => {
+	jsdom.env({
+		url: 'http://fast.com/',
+		scripts: ['http://code.jquery.com/jquery.js'],
+		userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+		strictSSL: false,
+		features: {
+			FetchExternalResources: ['script'],
+			ProcessExternalResources: ['script']
+		},
+		done: (err, window) => {
+			if (err) {
+				return observer.error(err);
+			}
 
-		return {
-			speed: Number($('#speed-value').textContent),
-			unit: $('#speed-units').textContent.trim(),
-			// somehow it didn't work with `Boolean($('#speed-value.succeeded'))`
-			isDone: document.querySelectorAll('.succeeded').length > 0
-		};
-	})
-	.then(result => {
-		if (result.speed > 0 && result.speed !== prevSpeed) {
-			cb(null, result);
+			let speed;
+
+			const interval = setInterval(() => {
+				try {
+					const result = {
+						speed: Number(window.$('#speed-value').text().trim() || 0),
+						unit: window.$('#speed-units').text().trim()
+					};
+
+					if (speed !== result.speed) {
+						observer.next(result);
+						speed = result.speed;
+					}
+
+					const isDone = window.$('.succeeded').length > 0;
+
+					if (isDone) {
+						clearInterval(interval);
+						observer.complete();
+					}
+				} catch (err) {
+					clearInterval(interval);
+					observer.error(err);
+				}
+			}, 100);
 		}
-
-		if (result.isDone) {
-			page.close();
-		} else {
-			setTimeout(init, 100, page, cb, result.speed);
-		}
-	})
-	.catch(cb);
-}
-
-// TODO: use an event for progress and return a promise for completion
-module.exports = cb => {
-	driver.create({path: phantomjs.path})
-		.then(phantom => phantom.createPage())
-		.then(page => page.open('http://fast.com').then(() => {
-			init(page, cb);
-		}))
-		.catch(cb);
-};
+	});
+});
