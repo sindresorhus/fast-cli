08b3cf782aa715510617ec9d51577dd8147367c3
Uliya Poda <s0verina@gmail.com>
2021-05-07 21:32:32 +0300

    Migrate to Ink (#55)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- cli.js
+++ cli.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 'use strict';
-const dns = require('dns');
 const meow = require('meow');
-const chalk = require('chalk');
-const logUpdate = require('log-update');
-const ora = require('ora');
-const api = require('./api');
+const importJsx = require('import-jsx');
+const React = require('react');
+const {render} = require('ink');
+
+const ui = importJsx('./ui');
 
 const cli = meow(`
 	Usage
@@ -32,81 +32,13 @@ const cli = meow(`
 	}
 });
 
-// Check connections
-dns.lookup('fast.com', error => {
-	if (error && error.code === 'ENOTFOUND') {
-		console.error(
-			chalk.red(
-				`${lineBreak(1)}${spacing(1)}Please check your internet connection.${lineBreak(1)}`
-			)
-		);
-		process.exit(1);
-	}
-});
+const main = async () => {
+	const app = render(React.createElement(ui, {
+		singleLine: cli.flags.singleLine,
+		upload: cli.flags.upload
+	}));
 
-let data = {};
-const spinner = ora();
+	await app.waitUntilExit();
+};
 
-const lineBreak = amount => (cli.flags.singleLine ? '' : '\n'.repeat(amount));
-const spacing = amount => (cli.flags.singleLine ? '' : ' '.repeat(amount));
-
-const downloadSpeed = () =>
-	`${data.downloadSpeed} ${chalk.dim(data.downloadUnit)} ↓`;
-
-const uploadSpeed = () =>
-	data.uploadSpeed ?
-		`${data.uploadSpeed} ${chalk.dim(data.uploadUnit)} ↑` :
-		chalk.dim('- Mbps ↑');
-
-const uploadColor = string => (data.isDone ? chalk.green(string) : chalk.cyan(string));
-
-const downloadColor = string => ((data.isDone || data.uploadSpeed) ? chalk.green(string) : chalk.cyan(string));
-
-const speedText = () =>
-	cli.flags.upload ?
-		`${downloadColor(downloadSpeed())} ${chalk.dim('/')} ${uploadColor(uploadSpeed())}` :
-		downloadColor(downloadSpeed());
-
-const speed = () => speedText() + lineBreak(2);
-
-function exit() {
-	if (process.stdout.isTTY) {
-		logUpdate(`${lineBreak(2)}${spacing(4)}${speed()}`);
-	} else {
-		let output = `${data.downloadSpeed} ${data.downloadUnit}`;
-
-		if (cli.flags.upload) {
-			output += `\n${data.uploadSpeed} ${data.uploadUnit}`;
-		}
-
-		console.log(output);
-	}
-
-	process.exit();
-}
-
-if (process.stdout.isTTY) {
-	setInterval(() => {
-		const pre = lineBreak(2) + spacing(2) + chalk.gray.dim(spinner.frame());
-
-		if (!data.downloadSpeed) {
-			logUpdate(pre + lineBreak(2));
-			return;
-		}
-
-		logUpdate(pre + speed());
-	}, 50);
-}
-
-(async () => {
-	try {
-		await api({measureUpload: cli.flags.upload}).forEach(result => {
-			data = result;
-		});
-
-		exit();
-	} catch (error) {
-		console.error(error.message);
-		process.exit(1);
-	}
-})();
+main();
---
+++ ui.js
@@ -0,0 +1,139 @@
+'use strict';
+const dns = require('dns');
+const React = require('react');
+const {useState, useEffect} = require('react');
+const {Box, Text, Newline, useApp} = require('ink');
+const Spinner = require('ink-spinner').default;
+
+const api = require('./api');
+
+const FixedSpacer = ({size}) => (
+	<>{' '.repeat(size)}</>
+);
+
+const ErrorMessage = ({text}) => (
+	<Box>
+		<Text bold color="red">
+			›
+			<FixedSpacer size={1}/>
+		</Text>
+		<Text dimColor>
+			{text}
+		</Text>
+		<Newline count={2}/>
+	</Box>
+);
+
+const Spacer = ({singleLine}) => {
+	if (singleLine) {
+		return null;
+	}
+
+	return (
+		<Text>
+			<Newline count={1}/>
+		</Text>
+	);
+};
+
+const DownloadSpeed = ({isDone, downloadSpeed, uploadSpeed, downloadUnit} = {}) => {
+	const color = (isDone || uploadSpeed) ? 'green' : 'cyan';
+
+	return (
+		<Text color={color}>
+			{downloadSpeed}
+			<FixedSpacer size={1}/>
+			<Text dimColor>{downloadUnit}</Text>
+			<FixedSpacer size={1}/>
+			↓
+		</Text>
+	);
+};
+
+const UploadSpeed = ({isDone, uploadSpeed, uploadUnit} = {}) => {
+	const color = isDone ? 'green' : 'cyan';
+
+	if (uploadSpeed) {
+		return (
+			<Text color={color}>
+				{uploadSpeed}
+				<Text dimColor>
+					{` ${uploadUnit} ↑`}
+				</Text>
+			</Text>
+		);
+	}
+
+	return <Text dimColor color={color}>{' - Mbps ↑'}</Text>;
+};
+
+const Speed = ({upload, data}) => upload ? (
+	<>
+		<DownloadSpeed {...data}/>
+		<Text dimColor>{' / '}</Text>
+		<UploadSpeed {...data}/>
+	</>
+) : (<DownloadSpeed {...data}/>);
+
+const Fast = ({singleLine, upload}) => {
+	const [error, setError] = useState('');
+	const [data, setData] = useState({});
+	const [isDone, setIsDone] = useState(false);
+	const {exit} = useApp();
+
+	useEffect(() => {
+		dns.lookup('fast.com', error => {
+			if (error) {
+				setError(error.code === 'ENOTFOUND' ?
+					'Please check your internet connection' :
+					`Something happened ${JSON.stringify(error)}`
+				);
+				exit();
+				return;
+			}
+
+			api({measureUpload: upload}).forEach(result => {
+				setData(result);
+			}).catch(error2 => {
+				setError(error2.message);
+				exit();
+			});
+		});
+	}, []);
+
+	useEffect(() => {
+		if (data.isDone || (!upload && data.uploadSpeed)) {
+			setIsDone(true);
+		}
+	}, [data.isDone, upload, data.uploadSpeed]);
+
+	useEffect(() => {
+		if (isDone) {
+			exit();
+		}
+	}, [isDone, exit]);
+
+	if (error) {
+		return <ErrorMessage text={error}/>;
+	}
+
+	return (
+		<>
+			<Spacer singleLine={singleLine}/>
+			<Box>
+				{!isDone && (
+					<>
+						{!singleLine && <Text><FixedSpacer size={2}/></Text>}
+						<Text color="cyan"><Spinner/></Text>
+						<Text><FixedSpacer size={1}/></Text>
+					</>
+				)}
+				{isDone && <Text><FixedSpacer size={4}/></Text>}
+				{Object.keys(data).length !== 0 && <Speed upload={upload} data={data}/>}
+			</Box>
+			<Spacer singleLine={singleLine}/>
+		</>
+	);
+};
+
+module.exports = Fast;
